635c24bbfdf06fb36246de23269971c8b3f7dd6f
Boby900 <bobytiwari310@gmail.com>
2023-01-02 18:18:52 +0530

    added the project

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger")
+const navList = document.getElementById("nav-list")
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+hamburger.addEventListener('click',toggleButton)
